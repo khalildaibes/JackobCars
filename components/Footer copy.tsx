e506ad50db8ce25a5c42573ad77d259b243a24bf
khalildaibes <khalildaibes1@gmail.com>
2025-03-18 11:32:12 +0200

    fix issues

--- components/Footer copy.tsx
+++ components/Footer copy.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import { Button } from "../../components/ui/button";
-import { Input } from "../../components/ui/input";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 import { FacebookIcon, TwitterIcon, InstagramIcon, LinkedinIcon } from 'lucide-react';
 
 const Footer = () => {
