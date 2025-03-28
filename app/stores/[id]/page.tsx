8baa9e06e776da3cb21d65ffba367072cd52713e
khalildaibes <khalildaibes1@gmail.com>
2025-03-28 16:48:00 +0300

    asada

--- app/stores/[id]/page.tsx
+++ app/stores/[id]/page.tsx
@@ -11,7 +11,7 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 import { Img } from "../../../components/Img";
-import { CarCard } from "../../../components";
+import  CarCard  from "../../../components/CarCard";
 
 interface Store {
   id: number;
