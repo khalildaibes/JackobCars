a3a023c2e33777c147d622f5201654a71a1aabae
khalildaibes <khalildaibes1@gmail.com>
2025-05-01 11:15:41 +0300

    refactor: remove unused image rendering code in CardDetails component to streamline layout

--- components/CardDetails.tsx
+++ components/CardDetails.tsx
@@ -55,47 +55,8 @@ const CardDetails = ({ isOpen, closeModal, car }: cardDetailsProps) => {
                     />
                   </button>
 
-                  <div className="flex-1 flex flex-col gap-3">
-                    <div className="relative w-full h-40  bg-cover bg-center rounded-lg">
-                      <Image
-                        src={generateCarImageUrl(car)}
-                        alt="car model"
-                        fill
-                        priority
-                        className="object-contain"
-                      />
-                    </div>
-
-                    <div className="flex gap-3">
-                      <div className="flex-1 relative w-full h-24  rounded-lg">
-                        <Image
-                          src={generateCarImageUrl(car, "29")}
-                          alt="car model"
-                          fill
-                          priority
-                          className="object-contain"
-                        />
-                      </div>
-                      <div className="flex-1 relative w-full h-24  rounded-lg">
-                        <Image
-                          src={generateCarImageUrl(car, "33")}
-                          alt="car model"
-                          fill
-                          priority
-                          className="object-contain"
-                        />
-                      </div>
-                      <div className="flex-1 relative w-full h-24  rounded-lg">
-                        <Image
-                          src={generateCarImageUrl(car, "13")}
-                          alt="car model"
-                          fill
-                          priority
-                          className="object-contain"
-                        />
-                      </div>
-                    </div>
-                  </div>
+                 
+                  
 
                   <div className="flex-1 flex flex-col gap-2">
                     <h2 className="font-semibold text-xl capitalize">
