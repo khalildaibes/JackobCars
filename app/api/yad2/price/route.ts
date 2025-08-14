ea0558d8c02341ad4a1910813714abc64eb26d94
khalildaibes <khalildaibes1@gmail.com>
2025-08-14 09:03:27 +0300

    feat: add scraper API integration for improved data fetching from YAD2; enhance response logging and error handling in route.ts

--- app/api/yad2/price/route.ts
+++ app/api/yad2/price/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+import fetch from 'node-fetch';
 
 const YAD2_PRICE_BASE_URL = 'https://gw.yad2.co.il/price-list/calculate-price?';
 
@@ -18,12 +19,19 @@ export async function GET(request) {
     }
 
     const url = `${YAD2_PRICE_BASE_URL}modelMasterId=${subModelId}&kilometers=${kilometers}&ascentYearOnRoad=${ascentYearOnRoad}&ascentMonthOnRoad=${ascentMonthOnRoad}`;
+    fetch(`https://api.scraperapi.com/?api_key=0a06de12ff661cc5e1da2364c97be83b&url=${url}`)
+      .then(response => {
+        console.log("response is", response);
+        return NextResponse.json(response); 
+      })
+      .catch(error => {
+        console.log(error)
+      });
     const response = await fetch(url, {
       headers: {
         Accept: 'application/json, text/plain, */*',
         'User-Agent': 'Mozilla/5.0',
       },
-      cache: 'no-store',
     });
 
     if (!response.ok) {
